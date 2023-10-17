ed9da2ead6967fcc33e734c174207e055b710ed5
Samandar <samandarkutbiddinov403@gmail.com>
2023-10-17 21:19:39 +0500

    first

---
+++ index.js
@@ -0,0 +1,16 @@
+"use strict";
+
+console.log()
+
+let incr = 10;
+let decr = 10;
+
+incr++;
+decr--;
+
+console.log(2*4 === '8');
+
+const isChecked = true;
+const isClose = false;
+
+console.log(isChecked && isClose);
